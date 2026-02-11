12de21905bdcb6050e1c4de4c46a75263f416001
Daniel Mårtensson <daniel.martensson100@outlook.com>
2026-02-11 20:31:33 +0100

    Lägger till NOTICE

--- recipes-bsp/trusted-firmware-a/tf-a-stm32mp/clk-stm32mp2.c
+++ recipes-bsp/trusted-firmware-a/tf-a-stm32mp/clk-stm32mp2.c
@@ -735,6 +735,7 @@ static struct clk_oscillator_data stm32mp2_osc_data[] = {
 static const char *clk_stm32_get_oscillator_name(enum stm32_osc id)
 {
 	if (id < NB_OSCILLATOR) {
+		NOTICE("clk_stm32_get_oscillator_name: id = %i", id);
 		return stm32mp2_osc_data[id].name;
 	}
 
@@ -929,7 +930,7 @@ static void clk_stm32_osc_init(struct stm32_clk_priv *priv, int id)
 {
 	struct clk_oscillator_data *osc_data = clk_oscillator_get_data(priv, id);
 	const char *name = osc_data->name;
-
+	NOTICE("clk_stm32_osc_init: id = %i, name = %s", id, name);
 	osc_data->frequency = clk_stm32_get_dt_oscillator_frequency(name);
 }
 
